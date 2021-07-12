acb57ec38c75a3f540ff7d70ee7af82f30962a54
appa12-tech <vsthapa12@gmail.com>
2021-07-12 10:42:07 +0545

    logout , restart and shutdown

--- jarvis.py
+++ jarvis.py
@@ -2,6 +2,10 @@ import pyttsx3
 import datetime
 import speech_recognition as sr
 import pyaudio
+import wikipedia
+import smtplib
+import webbrowser as wb
+import os
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
@@ -47,7 +51,7 @@ def takecomand():
         audio = r.listen(source)
     try:
         print("Re")
-        query = r.recognize_google(audio,'en=US')
+        query = r.recognize_google(audio,language='en=US')
         print(query)
     except Exception as e:
         print(e)
@@ -55,6 +59,13 @@ def takecomand():
         return "None"
     return query
 #takecomand()
+def sendmail(to,context):
+    server = smtplib.SMTP('smtp.gmail.com'),587
+    server.ehlo()
+    server.starttls()
+    server.login("test@gmail.com","123test")
+    server.sendmail("test@gmail.com",to,context)
+    server.close()
 if __name__ =="__main__":
     wishme()
     while True:
@@ -66,3 +77,30 @@ if __name__ =="__main__":
             date()
         elif "offline" in query :
             quit()
+        elif "wikipedia" in query :
+            speak("Searching.......")
+            query = query.replace("wikipedia","")
+            result = wikipedia.summary(query,sentences = 2)
+            speak(result)
+        elif "send email" in query :
+            try:
+                speak("What should I say")
+                context = takecomand()
+                to ="17bcs3287.cu@gmail.com"
+                sendmail(to,context)
+                speak("Email send sucessfully")
+                #speak(context)
+            except Exception as e:
+                speak(e)
+                speak("Unale to send the message")
+        elif "search in chrome" in query:
+            speak("What should i search")
+            chromepath ="C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe%s"
+            search= takecomand().lower()
+            wb.get(chromepath).open_new_tab(search +".com")
+        elif "logout" in query:
+            os.system("shutdown -1")
+        elif "shutdown" in query:
+            os.system("shutdown /s /t 1")
+        elif "restart" in query:
+            os.system("shutdown /r /t 1")
